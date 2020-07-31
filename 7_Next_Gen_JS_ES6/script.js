1bdc52c0e233afd3fa641ae8b46643b0b309f02e
github username <cristi.nord@gmail.com>
2020-07-31 16:20:43 +0100

    ES6/Blocks/IIFE/String/ArrowFunc play

--- 7_Next_Gen_JS_ES6/script.js
+++ 7_Next_Gen_JS_ES6/script.js
@@ -1,34 +1,124 @@
-// Let and Const
+// ///////////////////////////////////////////////// Let and Const
+
+// //ES5
+// var name5 = 'Jane Smith';
+// var age5 = 23;
+// name5 = 'Jane Miller';
+// console.log(name5);
+
+// //ES6
+// const name6 = 'Jane Smith';
+// let age6 = 23;
+// name6 = 'Jane M'; //<----err can not reassign
+// console.log(name6);
+
+// //ES5
+// function driverLicence(passTest) {
+//   if (passTest) {
+//     var firstName = 'John';
+//     var yearOfBirth = 1990;
+//   }
+//   console.log(
+//     firstName + ' is born in: ' + yearOfBirth + ' and is allowed to drive'
+//   );
+// }
+
+// driverLicence(true);
+
+// let i = 23;
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// /////////////////////////////////////////Blocks and IIFS
+
+// //ES6
+// {
+//   const a = 1;
+//   let b = 2;
+//   var c = 3;
+// }
+// console.log(c);
+// console.log(b);
+// console.log(a);
+
+// ///////////////////////////////////////// Strings in ES6
+
+// let firstName = 'John';
+// let lastName = 'Smith';
+// const yearOfBirth = 1990;
+
+// function calcAge() {
+//   return 2020 - yearOfBirth;
+// }
+
+// //ES5
+// console.log(
+//   'This is ' +
+//     firstName +
+//     ' ' +
+//     lastName +
+//     '. He was born in ' +
+//     yearOfBirth +
+//     '. Today, he is ' +
+//     calcAge(yearOfBirth) +
+//     ' years old.'
+// );
+
+// //ES6
+
+// console.log();
+
+// console.log(
+//   `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(
+//     yearOfBirth
+//   )} years old.`
+// );
+
+// const n = `${firstName} ${lastName}`;
+
+// console.log(
+//   `This is ${n}. He was born in ${yearOfBirth}. Today, he is ${calcAge(
+//     yearOfBirth
+//   )} years old.`
+// );
+
+// //////////////////////////////////////// String Methods in ES6
+// //startsWith()
+// //endsWith()
+// //includes()
+// //repeat()
+// console.log(n); // -> John Smith
+// console.log(n.startsWith('J')); // -> true
+// console.log(n.endsWith('H')); // -> false
+// console.log(n.includes('i')); // -> true
+// console.log(n.repeat(5)); // -> John SmithJohn SmithJohn SmithJohn SmithJohn Smith
+
+////////////////////////////////////Arrow functions
+
+const years = [1990, 1991, 1992, 1993];
 
 //ES5
-var name5 = 'Jane Smith';
-var age5 = 23;
-name5 = 'Jane Miller';
-console.log(name5);
+var ages5 = years.map(function (cur, indx, arr) {
+  return 2020 - cur;
+});
+console.log(ages5);
 
 //ES6
-const name6 = 'Jane Smith';
-let age6 = 23;
-name6 = 'Jane M'; //<----err can not reassign
-console.log(name6);
 
-//ES5
-function driverLicence(passTest) {
-  if (passTest) {
-    var firstName = 'John';
-    var yearOfBirth = 1990;
-  }
-  console.log(
-    firstName + ' is born in: ' + yearOfBirth + ' and is allowed to drive'
-  );
-}
+let ages6 = years.map((cur) => 2020 - cur);
+console.log(ages6);
 
-driverLicence(true);
+const ages7 = years.map((el, indx) => `Age element ${indx}: ${2020 - el}`);
+console.log(ages7);
 
-let i = 23;
+const ages8 = years.map((el, indx) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${indx}: ${2020 - el}`;
+});
 
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-
-console.log(i);
+console.log(ages8);
