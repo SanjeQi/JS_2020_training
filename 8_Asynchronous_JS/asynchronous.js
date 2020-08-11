1b06938c946f8daebc66036af7c26b1a02c2ab4f
github username <cristi.nord@gmail.com>
2020-08-11 18:56:32 +0100

    From calback hell to Promise play

--- 8_Asynchronous_JS/asynchronous.js
+++ 8_Asynchronous_JS/asynchronous.js
@@ -1,25 +1,80 @@
-//---------------------------------------------Asynchronous JS with callbacks
-function getRecipe() {
-  setTimeout(() => {
-    const recipeID = [523, 883, 432, 974];
-    console.log(recipeID);
-    setTimeout(
-      (id) => {
-        const recipe = { title: 'Frash Tomato Pasta', cook: 'Bono' };
-        console.log(`${id}: ${recipe.title}`);
-        setTimeout(
-          (cook) => {
-            const recipe2 = { title: 'Italian Pizza', cook: 'Bono' };
-            console.log(recipe2);
-          },
-          1000,
-          recipe.cook
-        );
-      },
-      1000,
-      recipeID[2]
-    );
-  }, 3000);
-}
+//---------------------------------------------Asynchronous JS with callbacks--callback hell :))))
+// function getRecipe() {
+//   setTimeout(() => {
+//     const recipeID = [523, 883, 432, 974];
+//     console.log(recipeID);
+//     setTimeout(
+//       (id) => {
+//         const recipe = { title: 'Frash Tomato Pasta', cook: 'Bono' };
+//         console.log(`${id}: ${recipe.title}`);
+//         setTimeout(
+//           (cook) => {
+//             const recipe2 = { title: 'Italian Pizza', cook: 'Bono' };
+//             console.log(recipe2);
+//           },
+//           1000,
+//           recipe.cook
+//         );
+//       },
+//       1000,
+//       recipeID[2]
+//     );
+//   }, 3000);
+// }
 
-getRecipe();
+// getRecipe();
+
+//---------------------------------------Asynchronous Js with Promises
+
+// Create a promise
+const getIDs = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve([523, 883, 432, 974]);
+  }, 5000);
+});
+
+const getRecipe = (recID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: 'Frash Tomato Pasta',
+          cook: 'Bono',
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recID
+    );
+  });
+};
+
+const getRecipe2 = (cook) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (chef) => {
+        const recipe2 = { title: 'Italian Pizza', cook: 'Bono' };
+        resolve(`${chef}: ${recipe2.title}`);
+      },
+      1500,
+      cook
+    );
+  });
+};
+
+// Consume the promise
+getIDs
+  .then((IDs) => {
+    console.log(IDs);
+    return getRecipe(IDs[2]);
+  })
+  .then((recipe) => {
+    console.log(recipe);
+    return getRecipe2('Bono');
+  })
+  .then((recipe2) => {
+    console.log(recipe2);
+  })
+  .catch((error) => {
+    console.log('Something went wrong');
+  });
