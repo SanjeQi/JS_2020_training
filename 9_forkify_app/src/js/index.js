89e0d2cf1221c1675f03c42434a24fb739061f5a
github username <cristi.nord@gmail.com>
2020-08-14 18:56:38 +0100

    html,webback, babel_p1

---
+++ 9_forkify_app/src/js/index.js
@@ -0,0 +1,4 @@
+// Global App Controller
+import x from './test';
+
+console.log(`I imported ${x} from another module`);
---
+++ 9_forkify_app/src/js/test.js
@@ -0,0 +1,3 @@
+console.log('Imported module');
+
+export default 123456778;
