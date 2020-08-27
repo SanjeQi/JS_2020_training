92a50cd2cd30addc66d865429ad26b9fedae5fa4
github username <cristi.nord@gmail.com>
2020-08-27 14:23:10 +0100

    Like Controller Build

--- 9_forkify_app/src/js/index.js
+++ 9_forkify_app/src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
@@ -138,6 +139,32 @@ elements.shopping.addEventListener('click', (e) => {
   }
 });
 
+// // *************************************************** Like Controller **********************************
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id;
+
+  // User has NOT yet liked current recipe
+  if (!state.likes.isLiked(currentID)) {
+    // Add like to state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img
+    );
+    // Toggle like button
+    // Add like to the UI list
+    console.log(state.likes);
+    // User HAS  liked current recipe
+  } else {
+    // Remove like from state
+    state.likes.deleteLike(currentID);
+    // Toggle like button
+    // Remove like from UI list
+    console.log(state.likes);
+  }
+};
 // *************************** Handling recipe button clicks - increase and decrease servings
 elements.recipe.addEventListener('click', (e) => {
   //if target matches the button-decrease or any childs of btn-decrease
@@ -153,6 +180,10 @@ elements.recipe.addEventListener('click', (e) => {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
   } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    //Add ingredients to shopping list
     controlList();
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    // Like Controller
+    controlLike();
   }
 });
